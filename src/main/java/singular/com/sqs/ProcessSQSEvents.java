40923f25552eb672feaa2aecf3a8c6fdc9020965
eran cohen <eran.c@singular.net>
2018-12-08 09:12:04 +0200

    add monitoring

--- src/main/java/singular/com/sqs/ProcessSQSEvents.java
+++ src/main/java/singular/com/sqs/ProcessSQSEvents.java
@@ -5,19 +5,34 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 import com.google.gson.Gson;
+import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.StatsDClient;
+import io.sentry.Sentry;
+import io.sentry.SentryClient;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Future;
 
 
 public class ProcessSQSEvents implements RequestHandler<SQSEvent, Void> {
 
-    final static Logger logger = Logger.getLogger(ProcessSQSEvents.class);
+    //final static Logger logger = Logger.getLogger(ProcessSQSEvents.class);
+
+    private static final StatsDClient statsd = new NonBlockingStatsDClient(
+            "mparticle.sqs.",                          /* prefix to any stats; may be null or empty string */
+            System.getenv("DATADOG_HOSTNAME"),                        /* common case: localhost */
+            8125,                                 /* port */
+            new String[] {"tag:value"}            /* Datadog extension: Constant tags, always applied */
+    );
+
+    final static String SQSQueueURL = System.getenv("SQS_QUEUE_URL");
 
     static class Requests {
         public List<String> requests = new LinkedList();
@@ -26,29 +41,38 @@ public class ProcessSQSEvents implements RequestHandler<SQSEvent, Void> {
     @Override
     public Void handleRequest(SQSEvent event, Context context)
     {
-        System.out.println("starting2");
-        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+        Sentry.init();
 
-            for(SQSMessage msg : event.getRecords()) {
+        try (CloseableHttpAsyncClient client = HttpAsyncClients.createDefault()) {
+
+            List<SQSEvent.SQSMessage> records = event.getRecords();
+            for(SQSMessage msg : records) {
                 String body = msg.getBody();
                 Requests requests = new Gson().fromJson(body, Requests.class);
+                long start = System.currentTimeMillis();
                 for(String request : requests.requests) {
                     try {
                         HttpGet httpRequest = new HttpGet(request);
-                        client.execute(httpRequest);
+                        Future<HttpResponse> responseF = client.execute(httpRequest, null);
                     }
                     catch (Exception e) {
                         System.out.println("Exception occurred while processing request: " + e.getMessage());
                     }
                 }
+                long runTime = System.currentTimeMillis() - start;
+                statsd.histogram("batch.execution.time", runTime);
+                statsd.histogram("batch.size", requests.requests.size());
             }
+            statsd.histogram("lambda.batch.size", records.size());
 
         } catch (IOException e) {
+            statsd.incrementCounter("unexpected.errors");
+            Sentry.capture(e);
             //logger.error("Exception occurred", e);
             System.out.println("Exception occurred: " + e.getMessage());
-
         }
 
         return null;
     }
+
 }
